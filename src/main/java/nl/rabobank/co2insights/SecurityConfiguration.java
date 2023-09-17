2f089a812426a951d85de5bfcd50793a4e8359f8
Ruslan Forostianov <ruslan@forostianov.com>
2023-09-17 15:05:57 +0200

    Do not defer and user raw csrf token

--- src/main/java/nl/rabobank/co2insights/SecurityConfiguration.java
+++ src/main/java/nl/rabobank/co2insights/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package nl.rabobank.co2insights;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -8,18 +10,35 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.function.Supplier;
 
 @Configuration
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // See explanation here
+        // https://docs.spring.io/spring-security/reference/5.8/migration/servlet/exploits.html#_i_am_using_angularjs_or_another_javascript_framework
+        CsrfTokenRequestHandler requestHandler = (HttpServletRequest request, HttpServletResponse response,
+                Supplier< CsrfToken > deferredCsrfToken) -> {
+            //To create cookies on GET requests as well
+            deferredCsrfToken.get();
+            new XorCsrfTokenRequestAttributeHandler().handle(request, response, deferredCsrfToken);
+        };
         return http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(requestHandler)
+                )
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated())
